14c45d2e2b897065c6b06fb27c6c3514913f5cf7
Ininahazwe <yves.cri@gmail.com>
2022-01-21 21:33:26 +0100

    sort

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import UserList from "./pages/UserList";
 
-function App() {
+const App = () => {
   return (
-      <>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/coup-coeur" element={<UserList />} />
-                  <Route path="*" element={<Home/>} />
-              </Routes>
-          </BrowserRouter>
-      </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coup-de-coeur" element={<UserList />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+import Card from "../components/Card";
 import Header from "../components/Header";
 
 const UserList = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=4727de2783c74dcac9d1e5e6bf2d7acc&language=fr-FR`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Coup de coeur <span>💖</span>
+      </h2>
+      <div className="result">
+        {listData.length > 0 ? (
+          listData.map((movie) => <Card movie={movie} key={movie.id} />)
+        ) : (
+          <h2>Aucun coup de coeur pour le moment</h2>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
